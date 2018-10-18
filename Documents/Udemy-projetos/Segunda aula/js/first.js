0a47d11ea7e09677ffe1a48f71c9d66044e42b4d
Sara Koskinen <sakfouri@gmail.com>
2018-10-18 16:44:46 +0300

    second class firts lessons

---
+++ Documents/Udemy-projetos/Segunda aula/js/first.js
@@ -0,0 +1,21 @@
+//creat variables
+
+var name ="Juho";
+var surname="Koskinen";
+
+console.log("variables create in JS");
+
+function showNameConsole(){
+    console.log("show name:" + name +" "+ surname);
+}
+
+function showNameDifferent(){
+    console.log("show name" + surname + "   " + name);
+} 
+
+function eventosClick(){
+    alert("you click in the botton");
+}
+
+
+showNameConsole();
