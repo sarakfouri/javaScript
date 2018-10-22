e394a353f7252bb0ccaea1a8c77812a823e05ab3
Sara Koskinen <sakfouri@gmail.com>
2018-10-22 10:48:30 +0300

    second Hello

--- Documents/React-teste/my-app/src/App.js
+++ Documents/React-teste/my-app/src/App.js
@@ -8,17 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World!!! 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Hello World!
+          </h1>
         </header>
       </div>
     );
